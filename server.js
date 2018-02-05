e4f7a08fff711c18ac2e82728cf313a5ca65134a
Steven <stevenesammon@hotmail.com>
2018-02-05 15:01:18 -0500

    uses middleware

--- server.js
+++ server.js
@@ -5,13 +5,19 @@ const logger = require("morgan");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-// Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-// Send every request to the React app
-// Define any API routes before this runs
+app.use(logger("dev"));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.Promise = Promise;
+const mongoDB_URI = process.env.MONGODB_URI || "mongodb://localhost/#";
+mongoose.connect(mongoDB_URI, {
+});
+
 app.get("*", function(req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
