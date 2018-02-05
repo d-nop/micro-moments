f1c33e889b8fb2d7eb6a89f9a4bbe8e83a65baf2
Steven <stevenesammon@hotmail.com>
2018-02-05 15:04:46 -0500

    requires models in server

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const logger = require("morgan");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const db = require("./models");
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
