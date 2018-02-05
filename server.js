85f9363890995b2e11f484cd8906c94f8cc96722
Steven <stevenesammon@hotmail.com>
2018-02-05 13:52:23 -0500

    requires morgan and mongoose

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const path = require("path");
+const mongoose = require("mongoose");
+const logger = require("morgan");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
